fc24c86da0c61e289554250ad82673c875a339f4
naman_yadav <namanyadav@gmail.com>
2020-04-26 17:54:34 -0400

    event create success message

--- public/js/rsvp_client.js
+++ public/js/rsvp_client.js
@@ -1,18 +1,15 @@
 
 function create_new_event(el, e) {
-    // alert(`trigger ${e.target}`)
-    // e.preventDefault();
-    // alert(`creating new event; ${$(el).data('action')}`)
-    // $(el).submit()
     $.ajax({
         url: '/events/create',
         type: 'POST',
         data: $(el).serialize(),
         success: function (res) {
-            alert('succ')
+            alert('Event created successfully');
+            location.reload();
         },
         error: function (err) {
-            alert('fail')
+            alert('There was a problem in creating the event')
         }
     });
     return false
